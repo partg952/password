654ff72b5f4ea4ce35c048995195fa83f2d25343
parth <dankparth@gmail.com>
2021-02-21 23:01:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let button = document.getElementById('button')
+let password = document.getElementById('password')
+let numArray = []
+let charArray = ['A','B','C','C','D','E','F','G','H','I','J','K','L','M','N']
+let symbolArray = ['(','#','%','&','*','^','!']
+let array = []
+
+for(let i=0;i<=9;i++){
+    numArray.push(i)
+    console.log(numArray[i])
+}
+
+button.addEventListener('click' , function(){
+    
+    password.value = ''
+
+
+console.log('clicked')
+    for(let i=0;i<=5;i++){
+        let num = gen();
+    let char = gen2();
+    let sign = gen3();
+    console.log(char + num + sign)
+    array.push(num)
+    array.push(char)
+    array.push(sign)
+    }
+
+    
+    for(let i=0;i<=5;i++){
+        password.value += array[i]
+        
+    }
+    array = []
+})
+
+function gen(){
+    let num = Math.floor(Math.random()*numArray.length)
+    return num
+}
+function gen2(){
+    let char = Math.floor(Math.random()*charArray.length)
+    return charArray[char]
+}
+function gen3(){
+    let sign = Math.floor(Math.random()*symbolArray.length)
+    return symbolArray[sign]
+}
